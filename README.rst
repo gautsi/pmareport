4060ac432c14319ad62e980fbd384b998247c666
Gautam <gautam.sisodia@gmail.com>
2015-08-12 19:56:36 -0400

    first commit

---
+++ README.rst
@@ -0,0 +1,12 @@
+===============================
+PMA report
+===============================
+
+.. image:: https://img.shields.io/travis/gautsi/pmareport.svg
+        :target: https://travis-ci.org/gautsi/pmareport
+
+A report for PMA
+
+* Free software: BSD license
+* Documentation: https://pmareport.readthedocs.org.
+
---
+++ tests/test_pmareport.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_pmareport
+----------------------------------
+
+Tests for `pmareport` module.
+"""
+
+import unittest
+
+from pmareport import pmareport
+
+
+class TestPmareport(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
