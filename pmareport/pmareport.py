3ec5a9d985c7ab0a4bfb03c1dd9ee2764f28a972
Gautam <gautam.sisodia@gmail.com>
2015-08-13 11:40:04 -0400

    started pmareport.py

--- pmareport/pmareport.py
+++ pmareport/pmareport.py
@@ -1,1 +1,21 @@
 # -*- coding: utf-8 -*-
+'''Pma report analysis.'''
+
+import pandas as pd
+
+
+def read_data(fp='../data/pmadata.csv'):
+    return pd.read_csv(fp)
+
+
+class Clinic(object):
+    '''
+    A collection of functions for the pma report analysis.
+    '''
+
+    def __init__(self, df=None, fp='../data/pmadata.csv'):
+        if df is None:
+            self.df = read_data(fp)
+        else:
+            self.df = df
+        self.df = self.df.drop_duplicates()
