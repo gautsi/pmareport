f1b1d56ab780da234bffdc870376cbcb8b76e412
Gautam <gautam.sisodia@gmail.com>
2015-08-13 18:20:03 -0400

    adding features

--- pmareport/pmareport.py
+++ pmareport/pmareport.py
@@ -2,6 +2,7 @@
 '''Pma report analysis.'''
 
 import pandas as pd
+import collections
 
 
 def read_data(fp='../data/pmadata.csv'):
@@ -14,8 +15,51 @@ class Clinic(object):
     '''
 
     def __init__(self, df=None, fp='../data/pmadata.csv'):
+
         if df is None:
             self.df = read_data(fp)
         else:
             self.df = df
+
+        # drop the duplicates
         self.df = self.df.drop_duplicates()
+
+        # change time columns to datetime formats
+        self.df['date'] = pd.to_datetime(self.df.VISIT_DATE, format='%Y-%m-%d')
+        self.df['sched'] = pd.to_datetime(
+            self.df.PT_SCHEDULED_APPT,
+            format='%H:%M'
+            )
+        self.df['start'] = pd.to_datetime(
+            self.df.PT_START_TIME,
+            format='%H:%M'
+            )
+        self.df['end'] = pd.to_datetime(
+            self.df.PT_END_TIME,
+            format='%H:%M'
+            )
+        self.df['arrive'] = pd.to_datetime(
+            self.df.PT_ARRIVE_TIME,
+            format='%H:%M'
+            )
+
+        # derive some basic time features
+        self.df['lateness'] = self.df.arrive - self.df.sched
+        self.df['delay'] = (self.df.start - self.df.arrive).dt.seconds/60.0
+        self.df['appt_time'] = (self.df.end - self.df.start).dt.seconds/60.0
+        self.df['month'] = self.df.date.dt.month
+        self.df['dayofweek'] = self.df.date.dt.dayofweek
+
+        # add feature: number of appointments that day
+        num_appts = self.df.groupby('date').count().PATIENT_ID
+        self.df['num_appts'] = self.df.date.apply(lambda x: num_appts[x])
+
+    def print_counts(self, col):
+        c = collections.Counter(self.df[col])
+        for cat, count in c.most_common():
+            print_str = '{cat}\t{co}\t{per:.2f}'.format(
+                cat=cat[:5],
+                co=count,
+                per=100*float(count)/len(self.df)
+                )
+            print(print_str)
